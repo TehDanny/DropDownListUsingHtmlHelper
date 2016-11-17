05bbd793fa9ed90039e26aec86a706083e893ce9
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-11-17 19:43:46 +0100

    Done.

---
+++ DropDownListUsingHtmlHelper/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DropDownListUsingHtmlHelper.MvcApplication" Language="C#" %>
---
+++ DropDownListUsingHtmlHelper/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DropDownListUsingHtmlHelper.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        [Display(Name = "Name")]
+        public string StudentName { get; set; }
+        public Gender StudentGender { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
